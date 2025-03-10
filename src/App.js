cfe6ace0f9917fd1114774d683e98bb32def91c4
Hedzer <hedzer@protonmail.com>
2025-03-10 23:07:15 +0100

    feat: all vocab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useEventListener } from "usehooks-ts";
+
+import allCategories from "./data/_index";
+
+const INITIAL_LANGUAGE = "english";
 
 function App() {
+  const [currentWords, setCurrentWords] = useState([]);
+  const [language, setLanguage] = useState(INITIAL_LANGUAGE);
+  const [currentWordIndex, setCurrentWordIndex] = useState(null);
+
+  const onToggleLanguage = () => {
+    setLanguage(language === "english" ? "italian" : "english");
+  };
+
+  const areAllDone = (words) => words.every((w) => !!w.done);
+
+  const getRandomWord = (words) => {
+    setLanguage(INITIAL_LANGUAGE);
+    if (areAllDone(words)) return;
+    const index = Math.floor(Math.random() * words.length);
+    return words[index].done ? getRandomWord(words) : index;
+  };
+
+  const onRetryAll = () => {
+    setCurrentWords((words) => {
+      const updatedWords = words.map((word, i) => {
+        return { ...word, done: false, learned: false };
+      });
+      setCurrentWordIndex(getRandomWord(updatedWords));
+      return updatedWords;
+    });
+  };
+
+  const onRetryMistakes = () => {
+    setCurrentWords((words) => {
+      const updatedWords = words.map((word, i) => {
+        return { ...word, done: word.learned ? true : false };
+      });
+      setCurrentWordIndex(getRandomWord(updatedWords));
+      return updatedWords;
+    });
+  };
+
+  const onKnow = () => {
+    setCurrentWords((words) => {
+      const updatedWords = words.map((word, i) => {
+        return i !== currentWordIndex ? word : { ...word, done: true, learned: true };
+      });
+      setCurrentWordIndex(getRandomWord(updatedWords));
+      return updatedWords;
+    });
+  };
+  const onDontKNow = () => {
+    setCurrentWords((words) => {
+      const updatedWords = words.map((word, i) => {
+        return i !== currentWordIndex ? word : { ...word, done: true };
+      });
+      setCurrentWordIndex(getRandomWord(updatedWords));
+      return updatedWords;
+    });
+  };
+
+  const onSetCategory = (i) => {
+    setCurrentWords(allCategories[i].Words);
+    setCurrentWordIndex(getRandomWord(allCategories[i].Words));
+  };
+
+  const allWords = allCategories.reduce((total, category) => (total += category.Words.length), 0);
+
+  useEventListener("keydown", (event) => {
+    if (event.key === "z") {
+      onDontKNow();
+    } else if (event.key === "x") {
+      onKnow();
+    } else if (event.key === " ") {
+      event.preventDefault();
+      onToggleLanguage();
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="category-list">
+        {allCategories.map((category, i) => (
+          <button onClick={() => onSetCategory(i)}>
+            {`${category.Name} (${category.Words.length})`}
+          </button>
+        ))}
+        <div>Total words: {allWords}</div>
+      </div>
+      <main>
+        <div className="start">
+          <button onClick={onRetryAll} disabled={!currentWords.length || !areAllDone(currentWords)}>
+            Retry All
+          </button>
+          <button
+            onClick={onRetryMistakes}
+            disabled={!currentWords.length || !areAllDone(currentWords)}
+          >
+            Retry Mistakes
+          </button>
+        </div>
+        {currentWordIndex !== null && (
+          <>
+            <div className="card" onClick={onToggleLanguage}>
+              {!areAllDone(currentWords) ? currentWords[currentWordIndex][language] : "Finito!"}
+            </div>
+
+            <div className="controls">
+              <button onClick={onDontKNow} disabled={areAllDone(currentWords)}>
+                Nope
+              </button>
+              <button onClick={onKnow} disabled={areAllDone(currentWords)}>
+                Got it
+              </button>
+            </div>
+
+            <div>
+              <div>Learned: {currentWords.filter((w) => w.learned).length}</div>
+              <div>Didn't know: {currentWords.filter((w) => !w.learned && w.done).length}</div>
+              <div>To Go: {currentWords.filter((w) => !w.done).length}</div>
+            </div>
+          </>
+        )}
+      </main>
     </div>
   );
 }
---
+++ src/data/_index.js
@@ -0,0 +1,77 @@
+import abstractNouns from "./abstractNouns.json";
+import adjectives from "./adjective.json";
+import adjectiveContrast from "./adjectiveContrast.json";
+import adverbs from "./adverbs.json";
+import animals from "./animals.json";
+import body from "./body.json";
+import business from "./business.json";
+import counting from "./counting.json";
+import commonNouns from "./commonNouns.json";
+import essentialVerbs from "./essentialVerbs.json";
+import foods from "./foods.json";
+import helperVerbs from "./helperVerbs.json";
+import houseObjects from "./houseObjects.json";
+import lyWords from "./lyWords.json";
+import actionsMental from "./actionsMental.json";
+import actionsAbstract from "./actionsAbstract.json";
+import actionsPhysical from "./actionsPhysical.json";
+import expressions from "./expressions.json";
+import actionsSenses from "./actionsSenses.json";
+import measurements from "./measurements.json";
+import nature from "./nature.json";
+import newOnes from "./new.json";
+import peoplesDescriptions from "./peoplesDescriptions.json";
+import peoplesOccupations from "./peoplesOccupations.json";
+import prepositions from "./prepositions.json";
+import prepositionSentences from "./prepositionSentences.json";
+import pronouns from "./pronouns.json";
+import shapesAndColors from "./shapesAndColors.json";
+import time from "./time.json";
+import travel from "./travel.json";
+import reflexiveVerbs from "./reflexiveVerbs.json";
+
+const nthNumber = 23
+const nthNumberNew = 2
+const newOnesWithProps = {
+  ...newOnes,
+  Words: newOnes.Words.map((word) => ({ ...word, learned: false, done: false })).filter(
+    // (word, index) => (index + 1) % 24 === nthNumber
+    (word, index) => (index + 1) % 3 === nthNumberNew && index > newOnes.Words.length - 100
+    // (word, index) => index > newOnes.Words.length - 70
+)};
+
+const allCategories = [
+  actionsMental,
+  actionsAbstract,
+  actionsPhysical,
+  actionsSenses,
+  adjectives,
+  adjectiveContrast,
+  abstractNouns,
+  adverbs,
+  animals,
+  body,
+  business,
+  counting,
+  commonNouns,
+  foods,
+  essentialVerbs,
+  expressions,
+  helperVerbs,
+  houseObjects,
+  lyWords,
+  measurements,
+  nature,
+  newOnesWithProps,
+  peoplesDescriptions,
+  peoplesOccupations,
+  prepositions,
+  prepositionSentences,
+  pronouns,
+  shapesAndColors,
+  time,
+  travel,
+  reflexiveVerbs
+];
+
+export default allCategories;
